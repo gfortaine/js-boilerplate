e87b2d893cc0fab08a03f394673756d2cad13434
Guillaume FORTAINE <guillaume@quocian.com>
2017-03-04 12:17:12 +0100

    add prettier-eslint for better compatibility between prettier and eslint

--- __tests__/kata.test.js
+++ __tests__/kata.test.js
@@ -1,10 +1,9 @@
-/* eslint-disable fp/no-unused-expression */
-/* eslint-disable better/explicit-return */
+/* eslint-disable fp/no-unused-expression, better/explicit-return */
 
-import kata from "../src/kata";
+import kata from '../src/kata';
 
-describe("kata", () => {
-  test("should do kata", () => {
-    expect(kata("value")).toBe("Toto");
+describe('kata', () => {
+  test('should do kata', () => {
+    expect(kata('value')).toBe('Toto');
   });
 });
--- src/kata.js
+++ src/kata.js
@@ -1,3 +1,3 @@
 // @flow
 
-export default () => "Toto";
+export default () => 'Toto';
